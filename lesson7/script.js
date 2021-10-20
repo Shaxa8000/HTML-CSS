14b8a0aa525883ad4ed0a150b2a407312f179779
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-10-20 11:48:16 +0500

    adding files

---
+++ lesson7/script.js
@@ -0,0 +1,9 @@
+const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+    
+}
+
+document.getElementById("demo2").innerHTML = fruits;
